dcde9e57c029a6fb836cb271110654a9fbbc86c9
jaehyeon48 <imjaehyeon48@gmail.com>
2021-08-18 21:50:39 +0900

    add NAVER search config

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
+          <meta name="naver-site-verification" content="857c5195b862aa111638e7fda2d8d162a81ba789" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
