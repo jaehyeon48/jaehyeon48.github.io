0029139a3ba64c6c89c224b139bc60e229a6a1f6
jaehyeon48 <imjaehyeon48@gmail.com>
2021-08-18 19:24:36 +0900

    move footer out of main tag

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { Top } from '../components/top'
 import { ThemeSwitch } from '../components/theme-switch'
 import { Footer } from '../components/footer'
-import { rhythm } from '../utils/typography'
 
 import './index.scss'
 
@@ -11,18 +10,11 @@ export const Layout = ({ title, children }) => {
   return (
     <React.Fragment>
       <Top title={title} />
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
+      <main className="main">
         <ThemeSwitch />
         {children}
-        <Footer />
-      </div>
+      </main>
+      <Footer />
     </React.Fragment>
   )
 }
