c6be71289103e338ccee87068046f252511f1911
jaehyeon48 <imjaehyeon48@gmail.com>
2021-08-18 21:51:00 +0900

    modify robots.txt and sitemap.xml url config

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,8 +96,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://your-blog.netlify.app',
-        sitemap: 'https://your-blog.netlify.app/sitemap.xml',
+        host: 'https://jaehyeon48.github.io',
+        sitemap: 'https://jaehyeon48.github.io/sitemap.xml',
         policy: [
           {
             userAgent: '*',
