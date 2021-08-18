6cefa41bd65a0e1dd4e3b79504b159c1a62839e6
jaehyeon48 <imjaehyeon48@gmail.com>
2021-08-18 17:38:28 +0900

    initial commit

---
+++ gatsby-meta-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: `Jaehyeon's Dev Blog`,
+  description: `개발자로서 성장해 나가는 것을 기록하는 블로그입니다. 개발을 통해 보다 나은 세상을 만드는데 기여하고 싶습니다.`,
+  author: `Jaehyeon Kim`,
+  introduction: ``,
+  siteUrl: `https://jaehyeon48.github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `jaehyeon48`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'jaehyeon48/blog-comments', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+}
