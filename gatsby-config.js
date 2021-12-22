00595a9ea06ed8c769f73db36b768e8db7f92523
jaehyeon48 <imjaehyeon48@gmail.com>
2021-12-22 19:07:48 +0900

    config: gatsby-remark-prismjs의 noInlineHighlight 설정 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,7 @@ module.exports = {
             resolve: `gatsby-remark-prismjs`,
             options: {
               inlineCodeMarker: '%',
+              noInlineHighlight: false,
             },
           },
           `gatsby-remark-copy-linked-files`,
