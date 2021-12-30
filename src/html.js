8d6e119d7d533a93c2951722d6239503d9cbf23d
jaehyeon48 <imjaehyeon48@gmail.com>
2021-12-30 22:35:56 +0900

    config: <noscript> 제거 및 <meta name='description' /> 추가

--- src/html.js
+++ src/html.js
@@ -13,12 +13,15 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
+          <meta
+            name="description"
+            content="개발하면서 배운 내용들을 정리해 놓은 공간입니다."
+          />
           <meta name="naver-site-verification" content="857c5195b862aa111638e7fda2d8d162a81ba789" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
-          <noscript>You need to enable JavaScript to run this app.</noscript>
           <div
             key={`body`}
             id="___gatsby"
