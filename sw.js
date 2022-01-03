723735f0d576c7d27a6be3f66c4862224662bacf
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-04 07:09:38 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,10 +26,10 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e5b5df51aa60f288692f.js"
+    "url": "webpack-runtime-14a7dccf1f9fe883b849.js"
   },
   {
-    "url": "styles.b833e77db951f93cef59.css"
+    "url": "styles.3d95db91e02a72ca34e9.css"
   },
   {
     "url": "styles-407fe62976dc5310c43e.js"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ef577419d6160662481ee722a589b61a"
+    "revision": "fed08bc8afe19d005bd58a0455112230"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -53,7 +53,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "b415e93d074e5dfaf06de480acab2c36"
+    "revision": "774e73609cb8050b5e6f7a02fe32f52b"
   },
   {
     "url": "polyfill-a0c75589a40cac95b858.js"
