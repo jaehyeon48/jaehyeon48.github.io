3906e34ce20c1b746b8aba2a90e5dbc4d1f36b91
jaehyeon48 <imjaehyeon48@gmail.com>
2022-01-07 16:41:05 +0900

    add: 404 page 추가

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,9 +11,11 @@ class NotFoundPage extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <Head title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Head title="Page Not Found" />
+        <div class="not-found-wrap">
+          <h1 >404 Page Not Found</h1>
+          <p>죄송합니다. 요청하신 페이지를 찾을 수 없습니다.</p>
+        </div>
       </Layout>
     )
   }
