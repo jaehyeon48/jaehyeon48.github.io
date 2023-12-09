4f92cd8ca87a8dd251b3de2fbd2aeadb99907794
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-12-09 15:51:31 +0900

    Initiate v2

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Jaehyeon48's Blog",
+  description: 'A personal blog by Jaehyeon Kim',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  )
+}
