b091866fd98d131d9a60700ca8768b80f85f64da
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-12-10 18:18:44 +0900

    Add categories page

--- app/posts/categories/page.tsx
+++ app/posts/categories/page.tsx
@@ -1,6 +1,6 @@
 import { getCategories } from '@/utils/get-categories'
 
-import { contentSection, headerTitle } from '@styles/patterns'
+import { contentSection, grid, headerTitle } from '@styles/patterns'
 import { css } from '@styles/css'
 import { Header } from '@components/header'
 
@@ -13,14 +13,57 @@ export default async function CategoriesPage() {
         <h1 className={headerTitle()}>Categories</h1>
       </Header>
       <section
-        className={css({
+        className={contentSection({
+          position: 'relative',
           width: '100%',
           backgroundColor: 'white',
         })}
       >
-        <ul className={contentSection()}>
+        <p
+          className={css({
+            width: '100%',
+            textAlign: 'right',
+            marginBottom: '30px',
+            fontSize: {
+              base: 16,
+              lg: 18,
+            },
+          })}
+        >
+          {categories.length} categories
+        </p>
+        <ul
+          className={grid({
+            gridTemplateColumns: {
+              base: '1fr',
+              md: 'repeat(2, 1fr)',
+            },
+            gap: 10,
+          })}
+        >
           {categories.map((category) => (
-            <li key={category}>{category}</li>
+            <li
+              key={category}
+              className={css({
+                backgroundColor: 'gray.100',
+                borderRadius: 6,
+                boxShadow: 'md',
+              })}
+            >
+              <a
+                href={`/posts/categories/${category}`}
+                className={css({
+                  display: 'block',
+                  padding: '24px',
+                  _hover: {
+                    color: 'emerald.600',
+                    fontWeight: 700,
+                  },
+                })}
+              >
+                {category}
+              </a>
+            </li>
           ))}
         </ul>
       </section>
